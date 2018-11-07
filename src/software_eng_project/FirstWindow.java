c5bcc465398af4341a5ac906a42e068ecc0a08bc
Barry Mulvey <barry.mulvey@ucdconnect.ie>
2018-11-07 17:30:11 +0000

    Updated notes, added First Window. Signed-off-by: Barry Mulvey <barry.mulvey@ucdconnect.ie>

---
+++ src/software_eng_project/FirstWindow.java
@@ -0,0 +1,9 @@
+package software_eng_project;
+
+import javax.swing.*;
+public class FirstWindow {
+	public static void main(String[] args) {
+		JFrame j = new JFrame();
+		j.setVisible(true);
+	}
+}
